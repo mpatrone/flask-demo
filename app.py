3a23903d22b11ec7923a39d8442e7e456090bbb5
Manuel Patrone <po75558@gmail.com>
2016-05-24 23:42:03 +0100

    Changed py again

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/hi')
-def main():
-    return '<h1>Hello Manuel</h1>'
-
 @app.route('/')
 def main():
     return 'Hi ! Im a Flask application.'
+
+@app.route('/hi')
+def hello():
+    return '<h1>Hello Manuel</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
