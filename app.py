cd2bffbd8871532ef86803c9478f16616af0d4a4
Manuel Patrone <po75558@gmail.com>
2016-05-24 23:35:18 +0100

    Changed py again

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
+@app.route('/hi')
+def main():
+    return '<h1>Hello Manuel</h1>'
+
 @app.route('/')
 def main():
     return 'Hi ! Im a Flask application.'
